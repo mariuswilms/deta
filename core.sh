cef94418af720e08b9f154c5c06617d986eaf537
David Persson <davidpersson@gmx.de>
2011-03-10 17:15:23 +0100

    Rename scope to role.

--- core.sh
+++ core.sh
@@ -13,41 +13,41 @@
 
 printf "[%5s] Module %s loaded.\n" "ok" "core"
 
-# @FUNCTION: use_scope
-# @USAGE: [scope]
+# @FUNCTION: role
+# @USAGE: [role]
 # @DESCRIPTION:
-# Maps an env (left) to scope provided
-# to this function (right). Detects scope by:
+# Maps an env (left) to role provided
+# to this function (right). Detects role by:
 # 1. Checking all available envs for a variable i.e.
-#    DEV_[scope]="y" will select DEV as the env.
+#    DEV_[role]="y" will select DEV as the role.
 # 2. Prompting the user to select from available env.
-use_scope() {
+role() {
 	local PAIR=$(set | grep -m 1 "_$1=y" );
 
 	if [[ -n $PAIR ]];  then
-		 _env_to_scope ${PAIR%_*} $1
+		 _env_to_role ${PAIR%_*} $1
 	else
 		for ENV_ in $(set | grep -E "^[A-Z]+_HOST="); do
 			local AVAIL+="${ENV_%_*} "
 		done
-		local PS3="Please select an env to map to scope $1: "
+		local PS3="Please select an env to map to role $1: "
 		select ENV in $AVAIL; do
-			_env_to_scope $ENV $1
+			_env_to_role $ENV $1
 			break
 		done
 	fi
-	printf "[%5s] Using scope %s.\n" "ok" $@
+	printf "[%5s] Using role %s.\n" "ok" $@
 }
 
-# @FUNCTION: _env_to_scope
-# @USAGE: [env] [scope]
+# @FUNCTION: _env_to_role
+# @USAGE: [env] [role]
 # @DESCRIPTION:
-# Maps all variables from env to scope.
-_env_to_scope() {
+# Maps all variables from env to role.
+_env_to_role() {
 	local IFS=$'\n'
 	for C in $(set | grep -E "^$1_"); do
 		eval ${C/$1_/$2_}
 	done
-	printf "[%5s] Mapped env %s to scope %s.\n" "ok" $@
+	printf "[%5s] Mapped env %s to role %s.\n" "ok" $@
 }
 
