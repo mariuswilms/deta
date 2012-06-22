5157f679fea3c713b1014b9be4ecbaca85b9810b
David Persson <davidpersson@gmx.de>
2012-06-22 16:13:14 +0200

    Updating copyright year.

--- transfer.sh
+++ transfer.sh
@@ -1,12 +1,12 @@
 #
 # deta
 #
-# Copyright (c) 2011 David Persson
+# Copyright (c) 2011-2012 David Persson
 #
 # Distributed under the terms of the MIT License.
 # Redistributions of files must retain the above copyright notice.
 #
-# @COPYRIGHT 2011 David Persson <nperson@gmx.de>
+# @COPYRIGHT 2011-2012 David Persson <nperson@gmx.de>
 # @LICENSE   http://www.opensource.org/licenses/mit-license.php The MIT License
 # @LINK      http://github.com/davidpersson/deta
 #
