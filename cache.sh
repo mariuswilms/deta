a3dbbdbc572808b1d79af69f7395ca1839898cc6
David Persson <davidpersson@gmx.de>
2013-06-29 06:16:38 +0200

    Reduce automatisms in cache functions.
    Host and PHP interpreter must now be given as arguments to the functions.
    Document new signature.

--- cache.sh
+++ cache.sh
@@ -16,33 +16,36 @@ msgok "Module %s loaded." "cache"
 # @FUNCTION: cache_flush_memcached
 # @USAGE: <memcached host> [memcached port]
 # @DESCRIPTION:
-# Flushes memcached cache entirely. The host and port are
-# hardcoded to localhost:11211
+# Flushes memcached cache entirely. The port if not given defaults to
+# default assigned memcached port 11211.
 cache_flush_memcached() {
-	msg "Flushing memcached cache."
+	host=$1
+	port=${2:-11211}
+
+	msg "Flushing memcached on %s:%d cache." $host $port
 
 	set +o errexit
-	(echo -e 'flush_all'; sleep 1) | telnet localhost 11211
+	(echo -e 'flush_all'; sleep 1) | telnet $host $port
 	set -o errexit
 }
 
 # @FUNCTION: cache_flush_phpstat
 # @USAGE: <php>
 # @DESCRIPTION:
-# Flushes the PHP stat cache. Requires a 'THIS' environment.
+# Flushes the local PHP stat cache.
 cache_flush_phpstat() {
 	msg "Clearing PHP stat cache."
-	$THIS_PHP -r "clearstatcache();"
+	$1 -r "clearstatcache();"
 }
 
 # @FUNCTION: cache_flush_apc
 # @USAGE: <php>
 # @DESCRIPTION:
-# Flushes the APC system and user caches. Needs PHP command with APC
-# extension to be available. Requires a 'THIS' environment.
+# Flushes the local APC system and user caches. Needs PHP
+# command with APC extension to be available.
 cache_flush_apc() {
-	if [[ $($THIS_PHP -m | grep -q apc) == 0 ]]; then
+	if [[ $($1 -m | grep -q apc) == 0 ]]; then
 		msg "Clearing APC system and user cache."
-		$THIS_PHP -r "apc_clear(); apc_clear('user');"
+		$1 -r "apc_clear(); apc_clear('user');"
 	fi
 }
